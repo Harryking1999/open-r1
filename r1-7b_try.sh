32d8729960f0d8729d2055f66c8523e6f75bc849
Harryking1999 <1823114030@qq.com>
2025-07-13 11:08:04 +0800

    add log

--- r1-7b_try.sh
+++ r1-7b_try.sh
@@ -1,4 +1,4 @@
-PYTHONPATH=/home/fuzhizhang.fzz/open-r1/src:$PYTHONPATH CUDA_VISIBLE_DEVICES=4,5 accelerate launch --main_process_port 29501\
+PYTHONPATH=/home/fuzhizhang.fzz/open-r1/src:$PYTHONPATH CUDA_VISIBLE_DEVICES=5 accelerate launch --main_process_port 29501\
     --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/grpo.py \
     --config recipes/Qwen2.5-3B/grpo/config_demo.yaml \
     --wandb_entity fuzhizhang-fzz-westlake-university --wandb_project try_openr1_qwen2.5_3b --run_name Qwen2.5-3B-GRPO --vllm_mode colocate
